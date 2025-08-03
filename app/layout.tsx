8fb20fb4b9f373575ce6f5011fdeae47ad6a9b1a
Erfan Azarmi <erfanazarmi1@gmail.com>
2025-08-03 19:31:53 +0330

    chore: add metadata for page titles and descriptions

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'A Next.js dashboard project based on the official Next.js tutorial.',
+  metadataBase: new URL('https://nextjs-dashboard-erfan.netlify.app'),
+};
 
 export default function RootLayout({
   children,
